9c1892adfe27994d06666fa373093b728fd32563
Vraj Patel <vrajpatel@Vrajs-MacBook-Pro.local>
2020-03-15 21:37:45 -0400

    Set up connection and cleaned up app.js and server.js

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const tournamentRoutes = require('./api/routes/tournaments');
+const tournamentRoutes = require("./api/routes/tournaments");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/tournaments', tournamentRoutes);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.use("/tournaments", tournamentRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Route Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.state = error.status || 500;
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const http = require('http');
+const http = require("http");
 
-const app = require('./app');
+const app = require("./app");
 
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 
 server.listen(port);
-
