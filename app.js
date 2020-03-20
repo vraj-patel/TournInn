d411fa5c85d098498645492ac57713620fd15a90
Vraj Patel <vrajpatel@Vrajs-MacBook-Pro.local>
2020-03-20 01:17:14 -0400

    Added user routes, password encryption, and web token

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const tournamentRoutes = require("./api/routes/tournaments");
+const userRoutes = require("./api/routes/user");
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/tournaments", tournamentRoutes);
+app.use("/user", userRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Route Not Found");
