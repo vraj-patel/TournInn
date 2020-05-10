6dbe95951ed4c7a0d9b87d9d2a5003268278fd85
Vraj Patel <vrajpatel@Vrajs-MacBook-Pro.local>
2020-05-10 16:40:43 -0400

    using async/await and promises to handle mysql fetches

--- api_server/api/controllers/predict.js
+++ api_server/api/controllers/predict.js
@@ -1,46 +1,47 @@
 const { PythonShell } = require("python-shell");
 const mysqlConnection = require("../../connection");
 
-exports.predictGame = (req, res, next) => {
-  team1Query = `
+getWinPerc = (teamId, tournId, seasonId) => {
+  query = `
   SELECT 
-    SUM(CASE WHEN (winnerId = ${req.params.team1Id} AND tournamentId = ${req.params.tournId} AND seasonId=${req.params.seasonId}) then 1 end) as teamWins, 
-    SUM(CASE when ((team1Id = ${req.params.team1Id} OR team2Id=${req.params.team1Id}) AND tournamentId = ${req.params.tournId} AND seasonId=${req.params.seasonId}) then 1 end) as totalGames 
-  FROM games`;
-  team2Query = `
-  SELECT 
-    SUM(CASE WHEN (winnerId = ${req.params.team2Id} AND tournamentId = ${req.params.tournId} AND seasonId=${req.params.seasonId}) then 1 end) as teamWins, 
-    SUM(CASE when ((team1Id = ${req.params.team2Id} OR team2Id=${req.params.team2Id}) AND tournamentId = ${req.params.tournId} AND seasonId=${req.params.seasonId}) then 1 end) as totalGames 
-  FROM games`;
-  team1Query2 = `SELECT wins, losses FROM teamStandings WHERE teamId = ${req.params.team1Id};`;
-  team2Query2 = `SELECT wins, losses FROM teamStandings WHERE teamId = ${req.params.team2Id};`;
+    SUM(CASE WHEN (winnerId = ${teamId} AND tournamentId = ${tournId} AND seasonId=${seasonId}) then 1 end) as teamWins, 
+    SUM(CASE when ((team1Id = ${teamId} OR team2Id=${teamId}) AND tournamentId = ${tournId} AND seasonId=${seasonId}) then 1 end) as totalGames 
+  FROM games;
+  SELECT wins, losses FROM teamStandings WHERE teamId = ${teamId};
+  `;
 
-  mysqlConnection.query(team1Query, (err, rows, fields) => {
-    team1CurrWinPerc = rows[0].teamWins / rows[0].totalGames;
+  return new Promise((resolve, reject) => {
+    mysqlConnection.query(query, (err, rows, fields) => {
+      if (err) throw err;
+      rows = JSON.parse(JSON.stringify(rows));
+      result1 = rows[0];
+      result2 = rows[1];
 
-    mysqlConnection.query(team1Query2, (err, rows, fields) => {
-      team1PrevWinPerc = rows[0].wins / (rows[0].wins + rows[0].losses);
+      currWinPerc = result1[0].teamWins / result1[0].totalGames;
+      prevWinPerc = result2[0].wins / (result2[0].wins + result2[0].losses);
+      winPerc = (prevWinPerc + 2 * currWinPerc) / 3;
 
-      mysqlConnection.query(team2Query, (err, rows, fields) => {
-        team2CurrWinPerc = rows[0].teamWins / rows[0].totalGames;
-
-        mysqlConnection.query(team2Query2, (err, rows, fields) => {
-          team2PrevWinPerc = rows[0].wins / (rows[0].wins + rows[0].losses);
-
-          tm1WinPerc = (team1PrevWinPerc + 2 * team1CurrWinPerc) / 3;
-          tm2WinPerc = (team2PrevWinPerc + 2 * team2CurrWinPerc) / 3;
-          const options = {
-            args: [tm1WinPerc, tm2WinPerc, req.params.isTeam1Home === "true" ? 1 : 0],
-          };
-
-          PythonShell.run("./api/machine_learning/predict_games.py", options, function (err, results) {
-            if (err) res.status(500).json({ error: err });
-            prediction = results[0];
-            parseInt(prediction) ? (prediction = req.params.team1Id) : (prediction = req.params.team2Id);
-            return res.status(200).json(prediction);
-          });
-        });
-      });
+      resolve(winPerc);
     });
+  });
+};
+
+exports.predictGame = async (req, res, next) => {
+  try {
+    tm1WinPerc = await getWinPerc(req.params.team1Id, req.params.tournId, req.params.seasonId);
+    tm2WinPerc = await getWinPerc(req.params.team2Id, req.params.tournId, req.params.seasonId);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+
+  const options = {
+    args: [tm1WinPerc, tm2WinPerc, req.params.isTeam1Home === "true" ? 1 : 0],
+  };
+
+  PythonShell.run("./api/machine_learning/predict_games.py", options, function (err, results) {
+    if (err) res.status(500).json({ error: err });
+    prediction = results[0];
+    parseInt(prediction) ? (prediction = req.params.team1Id) : (prediction = req.params.team2Id);
+    return res.status(200).json(prediction);
   });
 };
