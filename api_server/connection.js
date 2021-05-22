258493c82985b7abdd9cc699aa333692a58c149d
Vraj Patel <vrajpatel@Vrajs-MacBook-Pro.local>
2021-05-22 14:32:21 -0400

    added createTables.sql for correct db setup

--- api_server/connection.js
+++ api_server/connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 var mysqlConnection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASSWORD || require('./nodemon.json').env.DB_PASSWORD,
   database: "TournInn",
   multipleStatements: true,
 });
--- api_server/createTables.sql
+++ api_server/createTables.sql
@@ -1,8 +1,70 @@
-CREATE TABLE `TournInn`.`users` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `email` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  `role` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
+CREATE TABLE TournInn.users (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    email VARCHAR(320) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE TournInn.tournaments (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(255),
+    startTime DATETIME,
+    endTime DATETIME,
+    roundRobinDuration INT,
+    playoffDuration INT,
+    quarterFinalDuration INT,
+    semiFinalDuration INT,
+    finalDuration INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE TournInn.divisions (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    name VARCHAR(50) NOT NULL,
+    tournamentId INT NOT NULL,
+    FOREIGN KEY (tournamentId) REFERENCES TournInn.Tournaments(id)
+);
+
+CREATE TABLE TournInn.teams (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    name VARCHAR(50) NOT NULL,
+    divisionId INT NOT NULL,
+    FOREIGN KEY (divisionId) REFERENCES TournInn.divisions(id)
+);
+
+CREATE TABLE TournInn.players (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    name VARCHAR(50) NOT NULL,
+    teamId INT NOT NULL,
+    FOREIGN KEY (teamId) REFERENCES TournInn.teams(id)
+);
+
+CREATE TABLE TournInn.locations (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(255)
+);
+
+CREATE TABLE TournInn.games (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    sport VARCHAR(50) NOT NULL,
+    startTime DATETIME,
+    endTime DATETIME,
+    winnerId INT NOT NULL,
+    type VARCHAR(50),
+    team1Id INT NOT NULL,
+    team2Id INT NOT NULL,
+    team1Score INT NOT NULL,
+    team2Score INT NOT NULL,
+    locationId INT,
+    tournamentId INT,
+    seasonId INT,
+    homeTeamId INT,
+    FOREIGN KEY (winnerId) REFERENCES TournInn.teams(id),
+    FOREIGN KEY (team1Id) REFERENCES TournInn.teams(id),
+    FOREIGN KEY (team2Id) REFERENCES TournInn.teams(id),
+    FOREIGN KEY (homeTeamId) REFERENCES TournInn.teams(id),
+    FOREIGN KEY (locationId) REFERENCES TournInn.locations(id),
+    FOREIGN KEY (tournamentId) REFERENCES TournInn.tournaments(id)
+);
