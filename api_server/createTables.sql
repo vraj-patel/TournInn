297a33c35375f7f520308054e502dcbd5cbdbf0d
Vraj Patel <vrajpatel@Vrajs-MacBook-Pro.local>
2021-05-22 14:44:46 -0400

    added role column to users

--- api_server/createTables.sql
+++ api_server/createTables.sql
@@ -2,6 +2,7 @@ CREATE TABLE TournInn.users (
     id INT NOT NULL AUTO_INCREMENT UNIQUE,
     email VARCHAR(320) NOT NULL,
     password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
 );
 
